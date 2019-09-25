d24d2c981d0e95bf496c39aeefa6d40a5f7519ca
Cody-Hayes97 <hayesmichael2@gmail.com>
2019-09-25 14:21:42 -0700

    stretch goal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,72 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "./Header";
+import Datepick from "./Datepicker";
+import moment from "moment";
 
 function Body() {
   const [info, setInfo] = useState([]);
+  const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
+  const [count, setCount] = useState(1);
+
+  function handleDateSubtract() {
+    setDate(
+      moment()
+        .clone()
+        .subtract(count, "days")
+        .format("YYYY-MM-DD")
+    );
+    setCount(count + 1);
+  }
+
+  function handleDateAdd() {
+    setDate(
+      moment()
+        .clone()
+        .add(count, "days")
+        .format("YYYY-MM-DD")
+    );
+    setCount(count - 1);
+  }
+
   useEffect(() => {
     axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=DzlN59i0YZt0PrAnZUPiM3ScrJK4YkxS02vach8M&date=${date}`
+      )
       .then(response => {
         console.log(response);
         setInfo(response.data);
       });
-  }, []);
+  }, [date]);
   return (
     <div className="body-container">
       <Header date={info.date} />
       <div className="title">
         <h2>{info.title}</h2>
       </div>
-      <img src={info.url} className="image" />
+      <div className="date-comp">
+        <span className="date-picker">
+          <p>Select from Archives</p>
+          <button
+            onClick={() => {
+              handleDateSubtract();
+            }}
+          >
+            Previous Posts
+          </button>
+          {/* <Datepick /> */}
+          <button
+            onClick={() => {
+              handleDateAdd();
+            }}
+          >
+            Newer Posts
+          </button>
+        </span>
+      </div>
+
+      <img src={info.url} className="image" alt={info.title} />
 
       <p className="explanation">{info.explanation}</p>
     </div>
